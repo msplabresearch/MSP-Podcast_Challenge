fa781a08616d7fe7720647b71d83542e0dc67a4c
Lucas Goncalves <70816275+ilucasgoncalves@users.noreply.github.com>
2024-02-16 20:48:42 -0600

    Update run_dim.sh

--- run_dim.sh
+++ run_dim.sh
@@ -13,7 +13,15 @@ for seed in 7; do
         --epochs=20 \
         --pooling_type=${pool_type} \
         --model_path=model/dim_ser/wavLM_adamW/${seed} || exit 0;
-    
+
+    # Evaluation on Test3 and save results using with format required by challenge
+    python eval_dim_ser_test3.py \
+        --ssl_type=${ssl_type} \
+        --pooling_type=${pool_type} \
+        --model_path=model/dim_ser/wavLM_adamW/${seed}  \
+        --store_path=result/dim_ser/wavLM_adamW/${seed}.txt || exit 0;
+
+    # General evaluation code for sets with labels
     python eval_dim_ser.py \
         --ssl_type=${ssl_type} \
         --pooling_type=${pool_type} \
