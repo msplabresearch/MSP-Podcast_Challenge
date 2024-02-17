29ce5d18067cb5ef82558cb7d18778fa38c16f2a
Lucas Goncalves <70816275+ilucasgoncalves@users.noreply.github.com>
2024-02-16 20:43:54 -0600

    Update collate_fn.py
    created a custom collate for test3 evals

--- utils/dataset/collate_fn.py
+++ utils/dataset/collate_fn.py
@@ -25,3 +25,24 @@ def collate_fn_wav_lab_mask(batch):
         attention_mask[data_idx,:dur] = 1
     ## compute mask
     return total_wav, total_lab, attention_mask, total_utt
+
+
+def collate_fn_wav_test3(batch):
+    total_wav = []
+    total_dur = []
+    total_utt = []
+    for wav_data in batch:
+
+        wav, dur = wav_data[0]   
+        total_wav.append(torch.Tensor(wav))
+        total_dur.append(dur)
+        total_utt.append(wav_data[1])
+
+    total_wav = nn.utils.rnn.pad_sequence(total_wav, batch_first=True)
+    
+    max_dur = np.max(total_dur)
+    attention_mask = torch.zeros(total_wav.shape[0], max_dur)
+    for data_idx, dur in enumerate(total_dur):
+        attention_mask[data_idx,:dur] = 1
+    ## compute mask
+    return total_wav, attention_mask, total_utt
